608acb7062eaee58431f68408211d1b84ce2790f
RmariaS <raq_samba@hotmail.com>
2025-09-26 17:13:14 +0200

    LoginViewModel en LoginView

--- Grocery.App/ViewModels/GroceryListViewModel.cs
+++ Grocery.App/ViewModels/GroceryListViewModel.cs
@@ -4,36 +4,42 @@ using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
 
-namespace Grocery.App.ViewModels
+namespace Grocery.App.ViewModels;
+
+public partial class GroceryListViewModel : BaseViewModel
 {
-    public partial class GroceryListViewModel : BaseViewModel
+    public ObservableCollection<GroceryList> GroceryLists { get; set; }
+    private readonly IGroceryListService _groceryListService;
+    private readonly GlobalViewModel _global;
+
+    // Welkomstbericht property
+    public string WelcomeMessage => _global.Client != null ? $"Welkom {_global.Client.Name}!" : "";
+
+    public GroceryListViewModel(IGroceryListService groceryListService, GlobalViewModel global)
     {
-        public ObservableCollection<GroceryList> GroceryLists { get; set; }
-        private readonly IGroceryListService _groceryListService;
+        Title = "Boodschappenlijst";
+        _groceryListService = groceryListService;
+        _global = global;
 
-        public GroceryListViewModel(IGroceryListService groceryListService) 
-        {
-            Title = "Boodschappenlijst";
-            _groceryListService = groceryListService;
-            GroceryLists = new(_groceryListService.GetAll());
-        }
+        GroceryLists = new ObservableCollection<GroceryList>(_groceryListService.GetAll());
+    }
 
-        [RelayCommand]
-        public async Task SelectGroceryList(GroceryList groceryList)
-        {
-            Dictionary<string, object> paramater = new() { { nameof(GroceryList), groceryList } };
-            await Shell.Current.GoToAsync($"{nameof(Views.GroceryListItemsView)}?Titel={groceryList.Name}", true, paramater);
-        }
-        public override void OnAppearing()
-        {
-            base.OnAppearing();
-            GroceryLists = new(_groceryListService.GetAll());
-        }
+    [RelayCommand]
+    public async Task SelectGroceryList(GroceryList groceryList)
+    {
+        var paramater = new Dictionary<string, object> { { nameof(GroceryList), groceryList } };
+        await Shell.Current.GoToAsync($"{nameof(Views.GroceryListItemsView)}?Titel={groceryList.Name}", true, paramater);
+    }
 
-        public override void OnDisappearing()
-        {
-            base.OnDisappearing();
-            GroceryLists.Clear();
-        }
+    public override void OnAppearing()
+    {
+        base.OnAppearing();
+        GroceryLists = new ObservableCollection<GroceryList>(_groceryListService.GetAll());
+    }
+
+    public override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        GroceryLists.Clear();
     }
 }
